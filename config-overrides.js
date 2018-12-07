bd8f334abe4ac6e86ba26dc8dff51d659f5b1987
zhangxiaohu <xiaohu.zhang@dianrong.com>
2018-12-07 22:04:15 +0800

    less 支持

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,5 @@
 const { injectBabelPlugin } = require('react-app-rewired')
+const rewireLess = require('react-app-rewire-less')
 //px自动转换rem
 const px2rem = require('postcss-px2rem')
 module.exports = function override(config, env) {
@@ -13,17 +14,40 @@ module.exports = function override(config, env) {
     config
   )
 
+  config = rewireLess.withLoaderOptions({
+    modifyVars: {
+      '@primary-color': '#003580',
+      '@link-color': '#1890ff', // 链接色
+      '@font-size-base': '14px', // 主字号
+      '@text-color': 'rgba(0, 0, 0, .65)' // 主文本色
+    }
+  })(config, env)
+
+
   config.module.rules.push({
-    test: /\.css$/,
+    test: /\.less$/,
+    exclude: /node_modules/,
     use: [
+      // {
+      //   loader: 'style-loader'
+      // },
+      // {
+      //   loader: 'css-loader'
+      // },
       {
         loader: 'postcss-loader',
         options: {
-          plugins: () => [px2rem({ remUnit: 75 })]
+          plugins: () => [px2rem({ remUnit: 37.5 })]
         }
+      }
+      ,
+      {
+        loader: 'less-loader'
       }
     ]
   })
+
+
 
   // 装饰器
   injectBabelPlugin(
@@ -31,7 +55,7 @@ module.exports = function override(config, env) {
       '@babel/plugin-proposal-decorators',
       {
         // "decoratorsBeforeExport":true,
-        "legacy": true
+        legacy: true
       }
     ],
     config
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
-import {Button} from 'antd-mobile'
-import './HomePage.css'
-import { inject,observer } from 'mobx-react';
+import { Picker, List, Icon, DatePicker, Flex } from 'antd-mobile'
+import './HomePage.less'
+import { inject, observer } from 'mobx-react'
 
+const seasons = [
+  [
+    {
+      label: '2013',
+      value: '2013'
+    },
+    {
+      label: '2014',
+      value: '2014'
+    }
+  ],
+  [
+    {
+      label: '春',
+      value: '春'
+    },
+    {
+      label: '夏',
+      value: '夏'
+    }
+  ]
+]
 
-@inject('homePageStore') 
-@observer 
+@inject('homePageStore')
+@observer
 class HomePage extends Component {
-  componentDidMount(){
+  componentDidMount() {
     this.props.homePageStore.getList()
   }
 
@@ -15,17 +37,41 @@ class HomePage extends Component {
     this.props.homePageStore.add()
   }
   render() {
-    const {hotelList} = this.props.homePageStore || []
+    const minDate = new Date()
+    const { hotelList } = this.props.homePageStore || []
     return (
       <div>
-        <Button className="btn" onClick={this.handleAdd}>新增按钮</Button>
-        {
-          hotelList.map(item => <span className="hotel" key={item.id}>{item.hotelName}</span>)
-        }
+        <div className="search-container">
+          <h1>全球酒店官网优惠任您淘</h1>
+          <h4>官网预定更靠谱，更优惠......</h4>
+          <Picker
+            title="选择城市"
+            data={seasons}
+            cascade={false}
+            extra="请选择"
+          >
+            <List.Item arrow="horizontal">
+              <Icon type="search" />
+              目的地
+            </List.Item>
+          </Picker>
+
+          <Flex>
+            <Flex.Item>
+              <DatePicker mode="date" title="选择入住时间" extra="不限" minDate={minDate}>
+                <List.Item arrow="horizontal">入住时间</List.Item>
+              </DatePicker>
+            </Flex.Item>
+            <Flex.Item>
+              <DatePicker mode="date" title="选择退房时间" extra="不限" minDate={minDate}>
+                <List.Item arrow="horizontal">退房时间</List.Item>
+              </DatePicker>
+            </Flex.Item>
+          </Flex>
+        </div>
       </div>
     )
   }
 }
 
 export default HomePage
-
